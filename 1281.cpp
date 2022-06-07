9abc3f4ebfcd5938b74c725981846b2296df3130
sabbir <shawon.mu11@gmail.com>
2022-06-08 01:40:27 +0600

    subtract the product and sum of digits of an integer

--- 1281.cpp
+++ 1281.cpp
@@ -16,3 +16,21 @@ public:
         
     }
 };
+
+/*
+    n = 234   we have to take 2 3 4 individually 
+    if we do n%10 => 4. Found the last digit 
+    if we do n/10 => 23 
+        Again : 23%10 => 3  Found second digit
+                23/10 => 2  Found first digit 
+                    Again: 2%10 => 2 
+                           2/10 => 0 if n=0 program stops
+        
+    step 1 : find last digit n%10 
+    step 2 : multiply digit with prod 
+    step 3 : add digit with sum
+    step 4 : divide n by 10:  n= n/10 
+
+    Finally : subtract sum from prod 
+
+*/
